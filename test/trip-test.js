ef3aed12fbd3711f84e11fa776dde9b54670d502
Reid Poole <poole.reid@gmail.com>
2023-01-11 14:49:43 -0600

    Import classes and create decribe block and before each method

---
+++ test/trip-test.js
@@ -0,0 +1,12 @@
+const { expect } = require("chai");
+const Traveler = require("../src/Traveler")
+const Trips = require("../src/Trip")
+
+  describe("Trips", () => {
+    let trip
+    beforeEach(() => {
+      
+      trip = new Trips()
+    });
+ 
+  })
