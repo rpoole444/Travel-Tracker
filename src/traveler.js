4c0b48ac0f67c1419172b2990fb785c2f526bb7c
Reid Poole <poole.reid@gmail.com>
2023-01-11 17:26:09 -0600

    Add passing logic for methods

--- src/traveler.js
+++ src/traveler.js
@@ -1,22 +1,30 @@
+const dayjs = require('dayjs')
 class Traveler {
     constructor(travelerData, trips){
       this.id = travelerData.id
       this.name = travelerData.name
-      this.trips = trips
+      this.trips = trips.filter(obj =>obj.userID === this.id)
     }
 
-findPastTrips(){
-  
+
+getTripItinerary(time) {
+ const filteredTrip =this.trips.filter(trip => {
+    if(time === "past"){
+      return dayjs(trip.date).isBefore("2021/1/31")
+    } else if(time === "upcoming"){
+      return dayjs(trip.date).isAfter("2021/1/31")
+    } else if(time === "pending"){
+      return trip.status === "pending"
+    }
+  });
+  // console.log(filteredTrip)
+  return filteredTrip
 }
 
-findUpcomingTrips(){
 
+totalYearlySpent(){
+//need to calculate trom the trips data and include a tracel agents 10% fee
 }
-
-findPendingTrips(){
-
-}
-
 }
 
 
