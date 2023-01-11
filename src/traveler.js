de4862d3d208286bf27244519819d2d530291bfa
Reid Poole <poole.reid@gmail.com>
2023-01-11 14:48:14 -0600

    Add class constructor and properties for Traveler Class

---
+++ src/traveler.js
@@ -0,0 +1,23 @@
+class Traveler {
+    constructor(travelerData, trips){
+      this.id = travelerData.id
+      this.name = travelerData.name
+      this.trips = trips
+    }
+
+findPastTrips(){
+  
+}
+
+findUpcomingTrips(){
+
+}
+
+findPendingTrips(){
+
+}
+
+}
+
+
+module.exports = Traveler
