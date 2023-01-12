1a275a2781e774c7ba5aa09c2be04304428a698e
Reid Poole <poole.reid@gmail.com>
2023-01-12 12:53:31 -0600

    Import classes and dayjs

--- src/scripts.js
+++ src/scripts.js
@@ -3,8 +3,12 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
-
+import Traveler from '../src/traveler';
+import Destinations from '../src/Destinations';
+import Trip from '../src/Trip'
+import { Dayjs } from 'dayjs';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 // DOM RELATED, FETCH, POST, 
+
 console.log('This is the JavaScript entry file - your code begins here.');
