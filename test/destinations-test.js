e30795b1251c51d3d02436c291752d7a2461cd37
Reid Poole <poole.reid@gmail.com>
2023-01-14 17:01:10 -0600

    Add test for get by Name

--- test/destinations-test.js
+++ test/destinations-test.js
@@ -69,5 +69,17 @@ describe("Destinations", () => {
         "image": "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
         "alt": "overview of city buildings with a clear sky"
       })
+      
+    });
+  it("should get the destination by name", () => {
+    expect(destinationsRepo.getDestinationByName("Lima, Peru")).to.deep.equal({
+        "id": 1,
+        "destination": "Lima, Peru",
+        "estimatedLodgingCostPerDay": 70,
+        "estimatedFlightCostPerPerson": 400,
+        "image": "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
+        "alt": "overview of city buildings with a clear sky"
+      })
   });
+
 });
