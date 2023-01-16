4ebd9fed88480c8837ed01816f6f984ffffb815f
Reid Poole <poole.reid@gmail.com>
2023-01-16 01:18:21 -0600

    Delete Get By Name Method

--- test/destinations-test.js
+++ test/destinations-test.js
@@ -1,85 +1,82 @@
 const { expect } = require("chai");
 
-const Destinations = require("../src/Destinations")
+const Destinations = require("../src/Destinations");
 
 describe("Destinations", () => {
   let destinations;
   let destinationsRepo;
   beforeEach(() => {
-         destinations = [{
-        "id": 1,
-        "destination": "Lima, Peru",
-        "estimatedLodgingCostPerDay": 70,
-        "estimatedFlightCostPerPerson": 400,
-        "image": "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
-        "alt": "overview of city buildings with a clear sky"
-      }, {
-        "id": 2,
-        "destination": "Stockholm, Sweden",
-        "estimatedLodgingCostPerDay": 100,
-        "estimatedFlightCostPerPerson": 780,
-        "image": "https://images.unsplash.com/photo-1560089168-6516081f5bf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-        "alt": "city with boats on the water during the day time"
+    destinations = [
+      {
+        id: 1,
+        destination: "Lima, Peru",
+        estimatedLodgingCostPerDay: 70,
+        estimatedFlightCostPerPerson: 400,
+        image:
+          "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
+        alt: "overview of city buildings with a clear sky",
       },
       {
-        "id": 3,
-        "destination": "Sydney, Austrailia",
-        "estimatedLodgingCostPerDay": 130,
-        "estimatedFlightCostPerPerson": 950,
-        "image": "https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-        "alt": "opera house and city buildings on the water with boats"
+        id: 2,
+        destination: "Stockholm, Sweden",
+        estimatedLodgingCostPerDay: 100,
+        estimatedFlightCostPerPerson: 780,
+        image:
+          "https://images.unsplash.com/photo-1560089168-6516081f5bf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+        alt: "city with boats on the water during the day time",
       },
       {
-        "id": 4,
-        "destination": "c",
-        "estimatedLodgingCostPerDay": 65,
-        "estimatedFlightCostPerPerson": 350,
-        "image": "https://images.unsplash.com/photo-1558029697-a7ed1a4b94c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
-        "alt": "boats at a dock during the day time"
+        id: 3,
+        destination: "Sydney, Austrailia",
+        estimatedLodgingCostPerDay: 130,
+        estimatedFlightCostPerPerson: 950,
+        image:
+          "https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+        alt: "opera house and city buildings on the water with boats",
       },
       {
-        "id": 5,
-        "destination": "Madrid, Spain",
-        "estimatedLodgingCostPerDay": 150,
-        "estimatedFlightCostPerPerson": 650,
-        "image": "https://images.unsplash.com/photo-1543785734-4b6e564642f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-        "alt": "city with clear skys and a road in the day time"
-      }]
-      destinationsRepo = new Destinations(destinations)
+        id: 4,
+        destination: "c",
+        estimatedLodgingCostPerDay: 65,
+        estimatedFlightCostPerPerson: 350,
+        image:
+          "https://images.unsplash.com/photo-1558029697-a7ed1a4b94c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
+        alt: "boats at a dock during the day time",
+      },
+      {
+        id: 5,
+        destination: "Madrid, Spain",
+        estimatedLodgingCostPerDay: 150,
+        estimatedFlightCostPerPerson: 650,
+        image:
+          "https://images.unsplash.com/photo-1543785734-4b6e564642f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+        alt: "city with clear skys and a road in the day time",
+      },
+    ];
+    destinationsRepo = new Destinations(destinations);
   });
 
   it("should be a function", () => {
-    expect(Destinations).to.be.a("function")
+    expect(Destinations).to.be.a("function");
   });
 
   it("should be and instance of Destinations", () => {
-    expect(destinationsRepo).to.be.a.instanceOf(Destinations)
+    expect(destinationsRepo).to.be.a.instanceOf(Destinations);
   });
 
   it("should have destinations", () => {
-    expect(destinationsRepo.destinations).to.equal(destinations)
+    expect(destinationsRepo.destinations).to.equal(destinations);
   });
 
   it("should get the destination by ID", () => {
     expect(destinationsRepo.getDestinationById(1)).to.deep.equal({
-        "id": 1,
-        "destination": "Lima, Peru",
-        "estimatedLodgingCostPerDay": 70,
-        "estimatedFlightCostPerPerson": 400,
-        "image": "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
-        "alt": "overview of city buildings with a clear sky"
-      })
-      
+      id: 1,
+      destination: "Lima, Peru",
+      estimatedLodgingCostPerDay: 70,
+      estimatedFlightCostPerPerson: 400,
+      image:
+        "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
+      alt: "overview of city buildings with a clear sky",
     });
-  it("should get the destination by name", () => {
-    expect(destinationsRepo.getDestinationByName("Lima, Peru")).to.deep.equal({
-        "id": 1,
-        "destination": "Lima, Peru",
-        "estimatedLodgingCostPerDay": 70,
-        "estimatedFlightCostPerPerson": 400,
-        "image": "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
-        "alt": "overview of city buildings with a clear sky"
-      })
   });
-
 });
