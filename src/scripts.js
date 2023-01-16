f915f77457bb8b38e16b3fac8fa79f444ff89e67
Reid Poole <poole.reid@gmail.com>
2023-01-15 20:55:24 -0600

    Add jpg for packground

--- src/scripts.js
+++ src/scripts.js
@@ -1,23 +1,15 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS (SCSS) file
 import "./css/styles.css";
 import Traveler from "../src/traveler";
 import Destinations from "../src/Destinations";
 import Trip from "../src/Trip";
 import apiCalls from "../src/apiCalls";
 const dayjs = require("dayjs");
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-// import './images/turing-logo.png'
-// DOM RELATED, FETCH, POST,
 
 const greeting = document.querySelector(".greeting");
 const upcomingTrips = document.querySelector(".upcoming-trips");
-// const presentTrips = document.querySelector(".present-trips")
 const pastTrips = document.querySelector(".past-trips");
 const yearlyCost = document.querySelector(".total-spent-ty");
-const tripForm = document.querySelector(".input-field");
+// const tripForm = document.querySelector(".input-field");
 const destinationDropDown = document.querySelector("#destinationsDD");
 const lengthOfTripInput = document.querySelector("#lengthInput");
 const numberOfTravelersInput = document.querySelector("#travelersInput");
