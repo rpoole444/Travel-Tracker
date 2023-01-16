10a0bca48973ee32b979023926f08430b74ee147
Reid Poole <poole.reid@gmail.com>
2023-01-16 01:16:46 -0600

    Refactor properties

--- src/Trip.js
+++ src/Trip.js
@@ -1,18 +1,11 @@
 class Trip {
   constructor(trips) {
-    this.id = trips.id
-    this.userID = trips.userID
-    this.destinationID = trips.destinationID
-    this.travelers = trips.travelers
-    this.date = trips.date
-    this.duration = trips.duration
-    this.status = trips.status 
-    this.suggestedActivites = []//etc....
+    this.trips = trips;
   }
 
   getTripsByID(id) {
-    const tripsByID = this.trips.filter(trip => trip.userID === id)
-    return tripsByID
+    const tripsByID = this.trips.filter((trip) => trip.userID === id);
+    return tripsByID;
   }
 }
 module.exports = Trip;
